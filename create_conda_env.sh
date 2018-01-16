f7d93cfda39ee8f23412d8cf8e249e87aa96b82b
Kristian Gregorius Hustad <krihus@ifi.uio.no>
2018-01-16 19:36:37 +0100

    Use Python 3.6

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -4,7 +4,7 @@ CONDA_ENV_FILE=binder/environment.yml
 CONDA_PACKAGE_LIST=binder/conda_packages.txt
 set +x
 
-conda create --yes --name ${CONDA_ENV_NAME}
+conda create --yes --name ${CONDA_ENV_NAME} python=3.6
 source activate ${CONDA_ENV_NAME}
 
 conda config --append channels conda-forge
