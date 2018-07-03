68500bb5c2a4b24ac76e70543ea467a1eb8ee486
Kristian Gregorius Hustad <krihus@ifi.uio.no>
2018-07-03 14:58:50 +0200

    create_conda_env.sh: Exit on error

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -4,6 +4,9 @@ CONDA_ENV_FILE=binder/environment.yml
 CONDA_PACKAGE_LIST=binder/conda_packages.txt
 set +x
 
+# exit on non-zero return code
+set -e
+
 conda create --yes --name ${CONDA_ENV_NAME} python=3.6
 source activate ${CONDA_ENV_NAME}
 
